ad04d5d22ff7e83f847b496163ea01bec45a9e05
Shubham <shub.shukla@protonmail.com>
2026-01-01 23:41:57 +0530

    More switchmap example added

--- src/app/pages/rxjs-implementation/operators/switch-map/switch-map.component.ts
+++ src/app/pages/rxjs-implementation/operators/switch-map/switch-map.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { of, switchMap, timer, map } from 'rxjs';
+import { SwitchMapService } from './switch-map.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-switch-map',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './switch-map.component.html',
   styleUrl: './switch-map.component.scss'
 })
@@ -13,6 +15,9 @@ export class SwitchMapComponent {
   text$ = of('Hello');
 
   displayText: string = '';
+  userId: number = -1;
+
+  switchMapService = inject(SwitchMapService);
 
 
   onClick() {
@@ -21,4 +26,18 @@ export class SwitchMapComponent {
     });
   }
 
+  click2() {
+    let text = of('Welcome To');
+    let case1 = text.pipe(map((value) => value + ' Tutorialspoint!'));
+    case1.subscribe((value) => { console.log(value); });
+  }
+
+  findCityByUID() {
+    if (this.userId == -1) {
+      return;
+    }
+    this.switchMapService.getCityById(this.userId).subscribe((data) => {
+      console.log('city', data);
+    });
+  }
 }
--- src/app/pages/rxjs-implementation/operators/switch-map/switch-map.service.ts
+++ src/app/pages/rxjs-implementation/operators/switch-map/switch-map.service.ts
@@ -1,14 +1,62 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { filter, map, Observable, of, switchMap } from 'rxjs';
+
+export interface UserData {
+  id: number;
+  name: string;
+  age: number;
+  city: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SwitchMapService {
 
-  http = inject(HttpClient);
-
-
-
   constructor() { }
+
+  private userData: UserData[] = [
+    {
+      id: 1,
+      name: 'Shubham',
+      age: 22,
+      city: 'Mumbai'
+    },
+    {
+      id: 2,
+      name: 'Riya',
+      age: 22,
+      city: 'Surat'
+    },
+    {
+      id: 3,
+      name: 'Yug',
+      age: 22,
+      city: 'Indore'
+    },
+
+  ]
+
+  getUserNameById(id: number): Observable<string> {
+    const user = this.userData.find((u) => u.id === id);
+    return of(user).pipe(
+      filter((u): u is UserData => u !== undefined),
+      map((u) => u.name)
+    );
+  }
+
+  getUserCityByName(name: string): Observable<string> {
+    const user = this.userData.find((u) => u.name === name);
+    return of(user).pipe(
+      filter((u): u is UserData => u !== undefined),
+      map((u) => u.city)
+    );
+  }
+
+  getCityById(id: number): Observable<string> {
+    return this.getUserNameById(id).pipe(
+      switchMap((name) => this.getUserCityByName(name))
+    );
+  }
+
 }
